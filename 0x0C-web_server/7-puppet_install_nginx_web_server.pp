262b8e45e3e6850336d073ac70450cd69e3d4c6f
alishakor <aliuokeowo@gmail.com>
2023-04-27 05:10:18 +0100

    Installing Nginx using puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,6 @@
-#!/usr/bin/env bash
-# A Script to Install Nginx web server with puppet
+# Installing the Nginx web server
 
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
-}
-
-# Execute the Installation instructions
 exec {'install':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
   provider => shell,
-
-}
-
-exec {'Hello':
-  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-  provider => shell,
-}
-
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation /
-         \/redirect_me {\\n\\t\\treturn 301 https:\/\/redirect_me.allysnergy.com\/;\\n\\t}/" /
-         /etc/nginx/sites-available/default':
-  provider => shell,
-}
-
-exec {'run':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/alishakor permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }

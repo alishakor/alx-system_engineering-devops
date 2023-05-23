d775d1c6de2027ad05314a1dd58a560b43d7ee73
alishakor <aliuokeowo@gmail.com>
2023-05-23 10:56:46 +0100

    a python script that export file to json format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""a Python script that, using this REST API, for a given employee ID,
+"""
+   a Python script that, using this REST API, for a given employee ID,
    returns information about his/her TODO list progress
 
-   Extend the python script to export data in the CSV format
+   Extend the python script to export data in the json format
 
    REQUIREMENTS:
    Records all tasks that are owned by this employee
-   Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-   File name must be: USER_ID.csv
+   Format must be: { "USER_ID": [{"task": "TASK_TITLE", "completed":
+   TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task":
+   "TASK_TITLE", "completed": TASK_COMPLETED_STATUS,
+   "username": "USERNAME"}, ... ]}
+   File name must be: USER_ID.json
 
    """
 
-import csv
+import json
 import requests
 import sys
 
@@ -27,19 +31,23 @@ if __name__ == "__main__":
 
     if response_1.status_code == 200:
         user_id = response_1.json()
-        user_name = user_id.get("name")
+        user_name = user_id.get("username")
         user_task = response_2.json()
 
-        filename = f"{employee_id}.csv"
-        # Open the csv file in write mode
-        with open(filename, "w", newline="") as csv_file:
-            # Create a CSV writer object
-            writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        filename = f"{employee_id}.json"
+        # Open the json file in write mode
+        with open(filename, "w") as json_file:
+            task = []
             for task in user_task:
-                user_id = task.get("userId")
-                completed = task.get("completed")
-                title = task.get("title")
-                # Write the data to CSV file
-                writer.writerow([user_id, user_name, completed, title])
+                user_id = task["userId"]
+                completed = task["completed"]
+                title = task["title"]
+                task_dic = {
+                    "task": title,
+                    "completed": completed
+                    "username": user_name
+                }
+            # Write the data to json file
+            json.dump({user:id: task_list}, json_file}
     else:
         print(f"Error: {response_1.status_code}")

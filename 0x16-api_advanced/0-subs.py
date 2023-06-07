b8857b007a27798cc65e683cd4b08c957c9e4f03
alishakor <aliuokeowo@gmail.com>
2023-06-07 05:53:26 +0100

    a function that queries the REDDIT API and returns the number of REDDIT subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!usr/bin/python3
+"""A moodule that queries tne REDDIT API"""
+
+import requests
+
+
+def number_of_subscribers(subreddit)
+    """
+    a function that queries the REDDIT API
+
+    arg:
+        subreddit
+
+    Returns:
+        number of subscribers if True, else Zero
+    """
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            "User-Agent": "Subreddit subscriber"
+            }
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    if response.status_code == 200:
+        subscribers = data["data"]["subscribers"]
+    else:
+        subscribers = 0
+    return subscribers
